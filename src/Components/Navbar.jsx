cede0f914451363534b4d7c03ee36d109cc9b12a
rafatparween <rafatparween27392@gmail.com>
2025-01-20 09:15:10 +0530

    changes done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,26 @@
 // import { Box, Typography, IconButton } from "@mui/material";
 // import MenuIcon from "@mui/icons-material/Menu";
 // import { Link } from "react-router-dom";
+// import { styled } from "@mui/material/styles";
+// import CloseIcon from "@mui/icons-material/Close";
+
+// const NavbarBox = styled(Box)(({ theme }) => ({
+//   display: "none",
+//   alignItems: "center",
+//   [theme.breakpoints.up("sm")]: {
+//     display: "flex",
+//   },
+// }));
+
+// const HamburgerOrClose = styled(Box)(({ theme }) => ({
+//   display: "flex",
+//   justifyContent: "center",
+//   alignItems: "center",
+//   cursor: "pointer",
+//   [theme.breakpoints.up("sm")]: {
+//     display: "none",
+//   },
+// }));
 
 // const Navbar = () => {
 //   const [menuOpen, setMenuOpen] = useState(false);
@@ -47,21 +67,15 @@
 //           >
 //             Events Management
 //           </Typography>
-//           <IconButton
-//             onClick={toggleMenu}
-//             sx={{
-//               display: { xs: "block", sm: "none" },
-//               color: "white",
-//               fontSize: "1.5rem",
-//               marginLeft: "1rem",
-//             }}
-//           >
-//             <MenuIcon />
-//           </IconButton>
+//           <HamburgerOrClose>
+//             <IconButton onClick={toggleMenu} sx={{ color: "white", fontSize: "1.5rem" }}>
+//               {menuOpen ? <CloseIcon /> : <MenuIcon />}
+//             </IconButton>
+//           </HamburgerOrClose>
 //         </Box>
 
 //         {/* Navbar Menu */}
-//         <Box
+//         <NavbarBox
 //           sx={{
 //             display: { xs: menuOpen ? "flex" : "none", sm: "flex" },
 //             flexDirection: { xs: "column", sm: "row" },
@@ -74,25 +88,30 @@
 //             zIndex: 999,
 //           }}
 //         >
-//           {["Home", "About", "Events", "Gallery", "Contact", "Signup", "Login"].map(
-//             (item, index) => (
-//               <Typography
-//                 key={index}
-//                 component={Link}
-//                 to={`/${item.toLowerCase()}`}
-//                 sx={{
-//                   margin: { xs: "0.5rem 0", sm: "0 1rem" },
-//                   color: "white",
-//                   textDecoration: "none",
-//                   fontSize: { xs: "18px", sm: "20px" },
-//                   "&:hover": { color: "#ff69b4" },
-//                 }}
-//               >
-//                 {item}
-//               </Typography>
-//             )
-//           )}
-//         </Box>
+//           {[
+//             { name: "Home", path: "/" },
+//             { name: "About", path: "/About" },
+//             { name: "Events", path: "/Events" },
+//             { name: "Contact", path: "/Contact" },
+//             { name: "Signup", path: "/signup" },
+//             { name: "Login", path: "/Login" },
+//           ].map((item, index) => (
+//             <Typography
+//               key={index}
+//               component={Link}
+//               to={item.path}
+//               sx={{
+//                 margin: { xs: "0.5rem 0", sm: "0 1rem" },
+//                 color: "white",
+//                 textDecoration: "none",
+//                 fontSize: { xs: "18px", sm: "20px" },
+//                 "&:hover": { color: "#ff69b4" },
+//               }}
+//             >
+//               {item.name}
+//             </Typography>
+//           ))}
+//         </NavbarBox>
 //       </Box>
 //     </>
 //   );
@@ -103,9 +122,30 @@
 
 
 import React, { useState } from "react";
-import { Box, Typography, IconButton } from "@mui/material";
+import { Box, Typography, IconButton, Drawer, List, ListItem, ListItemText } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 import { Link } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+
+const NavbarBox = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const HamburgerOrClose = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  cursor: "pointer",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -150,38 +190,12 @@ const Navbar = () => {
           >
             Events Management
           </Typography>
-          <IconButton
-            onClick={toggleMenu}
-            sx={{
-              display: { xs: "block", sm: "none" },
-              color: "white",
-              fontSize: "1.5rem",
-              marginLeft: "1rem",
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
         </Box>
-
-        {/* Navbar Menu */}
-        <Box
-          sx={{
-            display: { xs: menuOpen ? "flex" : "none", sm: "flex" },
-            flexDirection: { xs: "column", sm: "row" },
-            alignItems: { xs: "center", sm: "center" },
-            position: { xs: "absolute", sm: "static" },
-            top: { xs: "60px", sm: "auto" },
-            left: { xs: 0, sm: "auto" },
-            backgroundColor: { xs: "black", sm: "transparent" },
-            width: { xs: "100%", sm: "auto" },
-            zIndex: 999,
-          }}
-        >
+        <NavbarBox>
           {[
             { name: "Home", path: "/" },
             { name: "About", path: "/About" },
             { name: "Events", path: "/Events" },
-            // { name: "Gallery", path: "/gallery" },
             { name: "Contact", path: "/Contact" },
             { name: "Signup", path: "/signup" },
             { name: "Login", path: "/Login" },
@@ -191,21 +205,62 @@ const Navbar = () => {
               component={Link}
               to={item.path}
               sx={{
-                margin: { xs: "0.5rem 0", sm: "0 1rem" },
+                margin: "0 1rem",
                 color: "white",
                 textDecoration: "none",
-                fontSize: { xs: "18px", sm: "20px" },
+                fontSize: "20px",
                 "&:hover": { color: "#ff69b4" },
               }}
             >
               {item.name}
             </Typography>
           ))}
-        </Box>
+        </NavbarBox>
+        <HamburgerOrClose>
+          <IconButton onClick={toggleMenu} sx={{ color: "white", fontSize: "1.5rem" }}>
+            <MenuIcon />
+          </IconButton>
+        </HamburgerOrClose>
       </Box>
+
+      <Drawer anchor="left" open={menuOpen} onClose={toggleMenu}>
+        <Box
+          sx={{
+            width: 250,
+            padding: "1rem",
+            backgroundColor: "#e5097f",
+            height: "100%",
+            color: "white",
+          }}
+        >
+          <IconButton onClick={toggleMenu} sx={{ color: "white", fontSize: "1.5rem" }}>
+            <CloseIcon />
+          </IconButton>
+          <List>
+            {[
+              { name: "Home", path: "/" },
+              { name: "About", path: "/About" },
+              { name: "Events", path: "/Events" },
+              { name: "Contact", path: "/Contact" },
+              { name: "Signup", path: "/signup" },
+              { name: "Login", path: "/Login" },
+            ].map((item, index) => (
+              <ListItem button key={index} component={Link} to={item.path} onClick={toggleMenu}>
+                <ListItemText
+                  primary={item.name}
+                  primaryTypographyProps={{
+                    fontSize: "20px",
+                    color: "white",
+                    textAlign: "center",
+                  }}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
     </>
   );
 };
 
 export default Navbar;
-
